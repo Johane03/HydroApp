6174375b9be59de4fef3462df2c364423eb0844f
Tashen <tashen1606@gmail.com>
2024-04-07 09:33:43 +0200

    First attempt at adding dashboard, main menu, etc.

--- app/src/main/java/com/example/hydroapp/MainMenu.java
+++ app/src/main/java/com/example/hydroapp/MainMenu.java
@@ -8,19 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        //hi guys, this is a test for changes. Please reply if you can see this :)
     }
 }
