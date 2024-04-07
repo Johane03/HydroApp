a19848aec92ac26ee1e79bb7f4bfeee8a4bd2409
Tashen <tashen1606@gmail.com>
2024-04-07 18:59:02 +0200

    Dashboard addition- v2

--- app/src/main/java/com/example/hydroapp/DashboardActivity.java
+++ app/src/main/java/com/example/hydroapp/DashboardActivity.java
@@ -1,6 +1,11 @@
 package com.example.hydroapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    WebView myWeb;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +27,24 @@ public class DashboardActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+
+        myWeb.setVerticalScrollBarEnabled(true);
+        myWeb.setHorizontalScrollBarEnabled(true);
+
+        myWeb.setWebChromeClient(new WebChromeClient());
+        myWeb.loadUrl("http://41.193.5.154:23500/ui");
+
+        btnBack = findViewById(R.id.Login);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
